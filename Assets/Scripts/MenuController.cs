31b2d7db742f0f8be684529329d66537a93ccb9b
Zachary Sarette <zacharywsarette@gmail.com>
2023-02-05 12:12:46 +0900

    Menu screen

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,22 +5,14 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject levelSelectMenu;
-
-    [Header("Buttons")]
-    [SerializeField]
-    private Button buttonSettings;
-
-    [SerializeField]
-    private GameObject buttonHolder;
-
-    [SerializeField]
-    private GameObject FocusButton;
-
 
     void Start()
     {
+    }
+
+    void OnStartGame()
+    {
+        SceneManager.LoadScene("Main");
     }
     
 
