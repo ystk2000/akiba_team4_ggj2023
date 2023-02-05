cd95e48733a654035fa025d19dcde230345ccdbf
Zachary Sarette <zacharywsarette@gmail.com>
2023-02-05 10:54:17 +0900

    Grass

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject levelSelectMenu;
+
+    [Header("Buttons")]
+    [SerializeField]
+    private Button buttonSettings;
+
+    [SerializeField]
+    private GameObject buttonHolder;
+
+    [SerializeField]
+    private GameObject FocusButton;
+
+
+    void Start()
+    {
+    }
+    
+
+}
